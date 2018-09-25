70073d78e09e4fe995276fb562691d27339643d1
Weiguang <chen.weiguang@gmail.com>
2018-09-25 11:28:25 +0800

    Initial implementation (#1)
    * Basic implementation with fixed EOF
    
    * Use EOF instead to end stdin entry
    
    * Add more details into README
    
    * Remove redundant comments

--- README.md
+++ README.md
@@ -1,2 +1,121 @@
 # secky
-SECrets KeYing Rust program to help keying in secrets into file or piped programs
+
+**SEC**rets **K**e**Y**ing Rust program to help keying in secrets into file or
+piped programs.
+
+The program aims to prevent secrets from being accidentally keyed in shell
+directly for shell history, and also prevent mistakes with the whitespaces being
+accidentally introduced at the end of the file.
+
+## Notes
+
+The program doesn't allow echoing into TTY for `stdout` unless `-f` is set.
+
+Press CTRL-D to end the text input. Auto-trimming is performed unless trim flags
+are set.
+
+Run `secky --help` for more program argument details.
+
+## Installation
+
+### Cargo
+
+```bash
+cargo install secky
+```
+
+## Command Examples
+
+### Print to `stdout`
+
+#### Method 1
+
+Command:
+
+```bash
+secky | cat
+```
+
+Input entry:
+
+```bash
+hello
+(CTRL-D)
+```
+
+Echo (no newline at the end):
+
+```bash
+hello
+```
+
+#### Method 2
+
+Command:
+
+```bash
+secky -f
+```
+
+Input entry:
+
+```bash
+hello
+(CTRL-D)
+```
+
+Echo (no newline at the end):
+
+```bash
+hello
+```
+
+### Print to file
+
+Command:
+
+```bash
+secky > secret.txt
+```
+
+Input entry:
+
+```bash
+hello
+(CTRL-D)
+```
+
+Check (no newline at the end):
+
+```bash
+cat secret.txt
+hello
+```
+
+### Pipe to other programs
+
+Command:
+
+```bash
+secky | md5sum
+```
+
+Input entry:
+
+```bash
+hello
+(CTRL-D)
+```
+
+Echo:
+
+```bash
+5d41402abc4b2a76b9719d911017c592  -
+```
+
+Check:
+
+```bash
+echo -n "hello" | md5sum
+5d41402abc4b2a76b9719d911017c592  -
+```
---
+++ src/main.rs
@@ -0,0 +1,75 @@
+#![deny(missing_debug_implementations, warnings)]
+
+extern crate atty;
+#[macro_use]
+extern crate failure;
+extern crate structopt;
+#[macro_use]
+extern crate structopt_derive;
+
+use atty::Stream;
+use std::io::Read;
+use std::{error::Error, process};
+use structopt::StructOpt;
+
+type MainResult = std::result::Result<(), Box<Error>>;
+
+/// Help keying in secrets into file or piped programs.
+///
+/// CTRL-D to end the text input, auto-trimming is performed unless trim flags
+/// are set.
+#[derive(StructOpt, Debug)]
+#[structopt(name = "secky")]
+struct Config {
+    /// Disable trimming of whitespaces at the beginning
+    #[structopt(short = "b", long = "no-trim-begin")]
+    no_trim_begin: bool,
+
+    /// Disable trimming of whitespaces at the end
+    #[structopt(short = "e", long = "no-trim-end")]
+    no_trim_end: bool,
+
+    /// Force allow echoing to TTY for stdout
+    #[structopt(short = "f", long = "force")]
+    force_stdout_tty: bool,
+}
+
+fn run() -> MainResult {
+    let config = Config::from_args();
+
+    // validation
+    let is_stdout_tty = atty::is(Stream::Stdout);
+
+    if is_stdout_tty && !config.force_stdout_tty {
+        Err(format_err!(
+            "Stdout TTY is not allowed. Redirect stdout or use -f."
+        ))?
+    }
+
+    // secret entry (CTRL-D to send EOF)
+    let mut secret = String::new();
+    std::io::stdin().read_to_string(&mut secret)?;
+
+    let secret = if config.no_trim_begin {
+        &secret
+    } else {
+        secret.trim_left()
+    };
+
+    let secret = if config.no_trim_end {
+        &secret
+    } else {
+        secret.trim_right()
+    };
+
+    print!("{}", secret);
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(e) = run() {
+        eprintln!("{}", e);
+        process::exit(1);
+    }
+}

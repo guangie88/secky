e8e41710c7b1c0fe3eab512816e93bd086c8a427
Chen Weiguang <chen.weiguang@gmail.com>
2018-09-25 11:35:13 +0800

    Add CI stuff

---
+++ install-linux.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+if [ `id -u` != 0 ]; then
+    echo "Run the script as root/sudo!"
+    exit 1
+fi
+
+REPO_USERNAME=guangie88
+REPO_NAME=secky
+
+LATEST_TAG=$(curl -sSf "https://api.github.com/repos/$REPO_USERNAME/$REPO_NAME/releases/latest" \
+    | grep tag_name \
+    | sed -n 's/.*"\(v.*\)".*/\1/p')
+
+BINARY_FILE=secky
+ZIP_SUFFIX=`uname -s | tr '[:upper:]' '[:lower:]'`-`uname -i`
+ZIP_FILE=$BINARY_FILE-$LATEST_TAG-$ZIP_SUFFIX.zip
+
+BIN_DIR=/usr/local/bin
+
+# unzip cannot work on Unix pipe
+echo "Downloading '"$ZIP_FILE"'..."
+curl -sSfLO "https://github.com/guangie88/$REPO_USERNAME/releases/download/$LATEST_TAG/$ZIP_FILE"
+
+echo "Unzipping..."
+unzip -qq "$ZIP_FILE" 
+rm "$ZIP_FILE"
+
+echo "Moving binary file '"$BINARY_FILE"' to $BIN_DIR/..."
+mv $BINARY_FILE $BIN_DIR/
+chmod +x $BIN_DIR/$BINARY_FILE
+
+echo "DONE!"
